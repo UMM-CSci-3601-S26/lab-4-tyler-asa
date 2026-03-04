2e98a73fc2f7c1a7bef4ec0c3cebee9da3b2242e
Thatcher Werner <werne662@morris.umn.edu>
2026-03-03 20:46:15 -0600

    Modified `cypress.config.ts` to potentially fix CI
    Modified the default command timeout to 10 seconds to potentially fix
    timeouts. Will revert if this doesn't work.

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -6,5 +6,6 @@ export default defineConfig({
       // eslint-disable-next-line @typescript-eslint/no-require-imports
       require('./cypress/plugins/index.ts').default(on, config);
     },
+    defaultCommandTimeout: 10000,
   },
 });
